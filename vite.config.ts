c756716b283644164bac443de0e321903ceea37c
FlorianDe <FlorianDe@users.noreply.github.com>
2024-11-19 22:01:14 +0100

    Added rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import process from "node:process";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import { displayNetworkUrlWithHostnamePlugin } from './config/vite-plugins/vite-display-network-url-hostname-plugin';
 import { selfSignedHttpsSupportPlugin } from './config/vite-plugins/vite-self-signed-https-support-plugin';
 
@@ -18,7 +19,14 @@ export default defineConfig({
         org: process.env.SENTRY_ORG,
         project: process.env.SENTRY_PROJECT,
         authToken: process.env.SENTRY_AUTH_TOKEN,
-        disable: isSentryDisabled // Seems like the sentry-vite-plugin not perfectly adapts to the disable option: https://github.com/getsentry/sentry-javascript-bundler-plugins/blob/main/packages/bundler-plugin-core/src/index.ts#L83-L101
+        disable: isSentryDisabled, // Seems like the sentry-vite-plugin not perfectly adapts to the disable option: https://github.com/getsentry/sentry-javascript-bundler-plugins/blob/main/packages/bundler-plugin-core/src/index.ts#L83-L101
+      }),
+      visualizer({
+        template: "treemap",
+        open: false,
+        gzipSize: true,
+        brotliSize: true,
+        filename: "generated/analyse.html",
       }),
       react(),
       displayNetworkUrlWithHostnamePlugin(),
